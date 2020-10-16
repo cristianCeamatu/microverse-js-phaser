1dc34991b88bda0b26bb2de1b34b175932e88f6f
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-10-16 17:39:18 +0300

    Basic game functionality with start collections and multiplayer

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const server = require('http').Server(app);
 const io = require('socket.io').listen(server);
 
 const players = {};
+const star = {
+  x: Math.floor(Math.random() * 700) + 50,
+  y: Math.floor(Math.random() * 500) + 50,
+};
+const scores = {
+  blue: 0,
+  red: 0,
+};
 
 app.use(express.static(`${__dirname}/dist`));
 
@@ -24,6 +32,10 @@ io.on('connection', (socket) => {
   };
 
   socket.emit('currentPlayers', players);
+  // send the star object to the new player
+  socket.emit('starLocation', star);
+  // send the current scores
+  socket.emit('scoreUpdate', scores);
 
   socket.broadcast.emit('newPlayer', players[socket.id]);
 
@@ -34,6 +46,27 @@ io.on('connection', (socket) => {
     delete players[socket.id];
     // emit a message to all players to remove this player
     io.emit('disconnect', socket.id);
+  });
+
+  // when a player moves, update the player data
+  socket.on('playerMovement', (movementData) => {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+  });
+
+  socket.on('starCollected', () => {
+    if (players[socket.id].team === 'red') {
+      scores.red += 10;
+    } else {
+      scores.blue += 10;
+    }
+    star.x = Math.floor(Math.random() * 700) + 50;
+    star.y = Math.floor(Math.random() * 500) + 50;
+    io.emit('starLocation', star);
+    io.emit('scoreUpdate', scores);
   });
 });
 
--- src/scenes/TemplateScene.js
+++ src/scenes/TemplateScene.js
@@ -1,7 +1,9 @@
 import Phaser from 'phaser';
 import io from 'socket.io-client';
 
-import player1 from '../assets/player1.png';
+import ship from '../assets/player1.png';
+import otherPlayer from '../assets/player2.png';
+import star from '../assets/star_gold.png';
 
 export default class TemplateScene extends Phaser.Scene {
   constructor() {
@@ -10,23 +12,156 @@ export default class TemplateScene extends Phaser.Scene {
 
   preload() {
     console.log('preload');
-    this.load.image('ship', player1);
+    this.load.image('ship', ship);
+    this.load.image('otherPlayer', otherPlayer);
+    this.load.image('star', star);
   }
 
   create() {
     const self = this;
     this.socket = io();
+    this.otherPlayers = this.physics.add.group();
+
     this.socket.on('currentPlayers', (players) => {
-      console.log(players);
       Object.keys(players).forEach((id) => {
         if (players[id].playerId === self.socket.id) {
-          // addPlayer(self, players[id]);
+          addPlayer(self, players[id]);
+        } else {
+          addOtherPlayers(self, players[id]);
         }
       });
+    });
+
+    this.socket.on('newPlayer', (playerInfo) => {
+      addOtherPlayers(self, playerInfo);
+    });
+
+    this.socket.on('disconnect', (playerId) => {
+      self.otherPlayers.getChildren().forEach((otherPlayer) => {
+        if (playerId === otherPlayer.playerId) {
+          otherPlayer.destroy();
+        }
+      });
+    });
+
+    this.socket.on('playerMoved', (playerInfo) => {
+      self.otherPlayers.getChildren().forEach((otherPlayer) => {
+        if (playerInfo.playerId === otherPlayer.playerId) {
+          otherPlayer.setRotation(playerInfo.rotation);
+          otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+        }
+      });
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.blueScoreText = this.add.text(16, 16, '', {
+      fontSize: '32px',
+      fill: '#0000FF',
+    });
+    this.redScoreText = this.add.text(584, 16, '', {
+      fontSize: '32px',
+      fill: '#FF0000',
+    });
+
+    this.socket.on('scoreUpdate', (scores) => {
+      self.blueScoreText.setText(`Blue: ${scores.blue}`);
+      self.redScoreText.setText(`Red: ${scores.red}`);
+    });
+
+    this.socket.on('starLocation', (starLocation) => {
+      if (self.star) self.star.destroy();
+      self.star = self.physics.add.image(
+        starLocation.x,
+        starLocation.y,
+        'star'
+      );
+      self.physics.add.overlap(
+        self.ship,
+        self.star,
+        function () {
+          this.socket.emit('starCollected');
+        },
+        null,
+        self
+      );
     });
   }
 
   update() {
-    // console.log('updating');
+    if (this.ship) {
+      if (this.cursors.left.isDown) {
+        this.ship.setAngularVelocity(-150);
+      } else if (this.cursors.right.isDown) {
+        this.ship.setAngularVelocity(150);
+      } else {
+        this.ship.setAngularVelocity(0);
+      }
+
+      if (this.cursors.up.isDown) {
+        this.physics.velocityFromRotation(
+          this.ship.rotation + 1.5,
+          100,
+          this.ship.body.acceleration
+        );
+      } else {
+        this.ship.setAcceleration(0);
+      }
+
+      this.physics.world.wrap(this.ship, 5);
+
+      // emit player movement
+      const { x } = this.ship;
+      const { y } = this.ship;
+      const r = this.ship.rotation;
+      if (
+        this.ship.oldPosition &&
+        (x !== this.ship.oldPosition.x ||
+          y !== this.ship.oldPosition.y ||
+          r !== this.ship.oldPosition.rotation)
+      ) {
+        this.socket.emit('playerMovement', {
+          x: this.ship.x,
+          y: this.ship.y,
+          rotation: this.ship.rotation,
+        });
+      }
+
+      // save old position data
+      this.ship.oldPosition = {
+        x: this.ship.x,
+        y: this.ship.y,
+        rotation: this.ship.rotation,
+      };
+    }
   }
 }
+
+const addPlayer = (self, playerInfo) => {
+  self.ship = self.physics.add
+    .image(playerInfo.x, playerInfo.y, 'ship')
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    self.ship.setTint(0x0000ff);
+  } else {
+    self.ship.setTint(0xff0000);
+  }
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
+};
+
+const addOtherPlayers = (self, playerInfo) => {
+  const otherPlayer = self.add
+    .sprite(playerInfo.x, playerInfo.y, 'otherPlayer')
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    otherPlayer.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
+};
